20bc6782a0ac9a20e8fb88d32e23b726b5597161
techzone11 <netflixyt78@gmail.com>
2023-03-01 12:40:29 +0530

    Initial commit

---
+++ src/cadence/scripts/getTotalSupply_script.js
@@ -0,0 +1,11 @@
+export const getTotalSupply =
+// REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
+`
+import CatMoji from 0x3dee3856bb07452b 
+
+pub fun main(): UInt64 {
+
+    return CatMoji.totalSupply;
+
+}
+`
